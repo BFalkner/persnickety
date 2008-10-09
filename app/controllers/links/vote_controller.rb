31b34acfbc47cda6e50e52b32bb87ed15f8ebee6
Brennan Falkner <fishy5@gmail.com>
2008-10-09 18:29:32 -0400

    Implement links/vote created.

--- app/controllers/links/vote_controller.rb
+++ app/controllers/links/vote_controller.rb
@@ -2,6 +2,12 @@ class Links::VoteController < ApplicationController
   before_filter :login_required, :only => [ :new, :create ], :redirect_to => "login_url"
 
   def create
+    @vote = Vote.new :user => current_user, :link => Link.find(params[:link_id])
+    if @vote.save
+      render :status => :created
+    else
+      render :status => :bad_request
+    end
   end
 
 end
--- test/functional/links/vote_controller_test.rb
+++ test/functional/links/vote_controller_test.rb
@@ -9,10 +9,35 @@ class Links::VoteControllerTest < Test::Unit::TestCase
     @response   = ActionController::TestResponse.new
   end
 
+  context "while logged in" do
+    setup do
+      login_as(:aaron)
+    end
+
+    context "on POST to create" do
+      context "with valid vote" do
+        setup do
+          post :create, :link_id => links(:one).id
+        end
+  
+        should_respond_with :created
+      end
+
+      context "with duplicate vote" do
+        setup do
+          Vote.create :user => users(:aaron), :link => links(:one)
+          post :create, :link_id => links(:one).id
+        end
+  
+        should_respond_with :bad_request
+      end
+    end
+  end
+
   context "while not logged in" do
     context "on POST to create" do
       setup do
-        post :create, :link_id => 1
+        post :create, :link_id => links(:one).id
       end
 
       should_redirect_to "login_url"
