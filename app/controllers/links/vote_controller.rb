f0d65296564b591724b72918ab20c465f3eac8cd
Brennan Falkner <fishy5@gmail.com>
2008-10-09 18:02:49 -0400

    Only allow creating votes when logged in.

--- app/controllers/links/vote_controller.rb
+++ app/controllers/links/vote_controller.rb
@@ -1,4 +1,6 @@
 class Links::VoteController < ApplicationController
+  before_filter :login_required, :only => [ :new, :create ], :redirect_to => "login_url"
+
   def create
   end
 
--- test/functional/links/vote_controller_test.rb
+++ test/functional/links/vote_controller_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
-class Links::VoteControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+class Links::VoteControllerTest < Test::Unit::TestCase
+  include AuthenticatedTestHelper
+
+  def setup
+    @controller = Links::VoteController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  context "while not logged in" do
+    context "on POST to create" do
+      setup do
+        post :create, :link_id => 1
+      end
+
+      should_redirect_to "login_url"
+    end
   end
 end
