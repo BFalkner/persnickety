c5359a3b275b7b4b81f320d3857b3d5c2e95e828
Brennan Falkner <fishy5@gmail.com>
2008-10-09 12:08:41 -0400

    Apply restful_authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,25 @@
 ActiveRecord::Schema.define(:version => 1) do
   create_table :links do |t|
     t.string    :title, :url
-    t.timestamp :created_at
+    t.datetime  :created_at
     t.integer   :creator_id
   end
 
   create_table :users do |t|
-    t.string    :name, :email, :password_hash
-    t.timestamp :created_at
+    t.string    :login, :limit => 40
+    t.string    :name, :limit => 100, :default => '', :null => true
+    t.string    :email, :limit => 100
+    t.string    :crypted_password, :limit => 40
+    t.string    :salt, :limit => 40
+    t.datetime  :created_at
+    t.datetime  :updated_at
+    t.string    :remember_token, :limit => 40
+    t.datetime  :remember_token_expires_at
   end
+  add_index :users, :login, :unique => true
   
   create_table :votes do |t|
     t.integer   :link_id, :user_id
-    t.timestamp :created_at
+    t.datetime  :created_at
   end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,11 +5,4 @@ class UserTest < Test::Unit::TestCase
 
   should_have_many :links, :through => :votes
   should_have_many :created
-  
-  should_require_attributes :email
-  should_require_unique_attributes :email
-  should_require_attributes :name
-  should_require_attributes :password_hash
-
-  should_protect_attributes :password_hash
 end
