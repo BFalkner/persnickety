58b4155b7a34b25ea84fc3f06580954f6c81d1f7
Brennan Falkner <fishy5@gmail.com>
2008-10-11 07:04:03 -0400

    Add Link#find_by_probability.
    I think there's something off with the math, but I'm not certain and it seems fairly accurate.

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,6 +7,43 @@ class Link < ActiveRecord::Base
   validates_presence_of :url
   validates_uniqueness_of :url
   validates_presence_of :creator_id
-  
+
   attr_protected :creator_id
+
+  def self.find_by_probability(user)
+    links = Link.find :all, :include => :votes
+    users = User.select { |u| u.votes.id }.to_a
+
+    links.map do |link|
+      link.probability = bayes(
+        link,
+        users_with_link(link, users),
+        proc{|e, h| common_links(e, user, h) / all_links(e, user, h) },
+        proc{|e| e.votes.size.to_f / links.size },
+        proc{|h| h.votes.size.to_f / users.size })
+      link
+    end
+  end
+
+  private
+
+  def self.all_links(lhs, rhs, link)
+    (lhs.votes.map(&:link_id) | rhs.votes.map(&:link_id) | [link.id]).size
+  end
+
+  def self.common_links(lhs, rhs, link)
+    ((lhs.votes.map(&:link_id) | [link.id]) & rhs.votes.map(&:link_id)).size.to_f
+  end
+
+  def self.users_with_link(link, users)
+    user_ids = link.votes.map(&:user_id)
+    users.select{|u| user_ids.include? u.id }
+  end
+
+  def self.bayes(hypothesis, evidence, conditional, marginal, prior)
+    evidence.map do |e|
+      conditional[e, hypothesis]/marginal[e]
+    end.inject(prior[hypothesis], &:*)
+  end
 end
+
